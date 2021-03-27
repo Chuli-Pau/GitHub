b905eba95aa05ff84ce2f4e9996e262e9c7ec613
Pau <paulasmv1513@gmail.comgit config --global user.name Paugit config --global user.email paulasmv1513@gmail.com>
2021-03-27 19:05:15 +0100

    saludo Java

---
+++ Java/src/GitJava.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author Pau
+ *
+ */
+public class GitJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+			System.out.println("Hola! como te llamas?");
+				String nombre= sc.nextLine();
+				
+			System.out.println("Encantado " + nombre + ", estamos aprendiendo a trabajar con Git");
+
+	}
+
+}
